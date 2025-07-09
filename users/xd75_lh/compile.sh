4b01a8d96921d3714f191770a3662d44cf41c9f4
Lihong <lihong.hs@protonmail.com>
2025-07-09 22:34:57 +0700

    update compile script

--- users/xd75_lh/compile.sh
+++ users/xd75_lh/compile.sh
@@ -5,25 +5,21 @@ set -e
 FILE=$1
 
 if [ -f "$FILE" ]; then
-    echo
-    echo "1) Converting keymap from json to c"
-    qmk json2c $FILE > ../../keyboards/xiudi/xd75/keymaps/lh/keymap.c
-    
-    echo
-    echo "2) Adding keymap_extra.c to keymap.c"
-    cat keymap_extra.c >> ../../keyboards/xiudi/xd75/keymaps/lh/keymap.c
 
-	echo
-    echo "3) Compiling"
-	echo
-    qmk compile -kb xiudi/xd75 -km lh
+  echo "1) Copying the keymap json"
+  cp $FILE $QMK_FIRMWARE_DIR/users/xd75_lh/keymap.json
+
+  echo "2) Converting keymap from json to c"
+  qmk json2c $FILE > $QMK_FIRMWARE_DIR/keyboards/xiudi/xd75/keymaps/lh/keymap.c
     
-	echo
-    echo "4) Done"
+  echo "3) Adding keymap_extra.c to keymap.c"
+  cat keymap_extra.c >> $QMK_FIRMWARE_DIR/keyboards/xiudi/xd75/keymaps/lh/keymap.c
+
+  echo "4) Compiling"
+  qmk compile -kb xiudi/xd75 -km lh
+    
+  echo "5) Done"
    
 else 
     echo "File named \"$FILE\" does not exist."
 fi
-
-
-
