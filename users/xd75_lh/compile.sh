2e9479e2ad22bcc9ffc9b3de886446f7d06206d1
Lihong <lihong.hs@protonmail.com>
2025-07-11 17:30:17 +0700

    update compile and flate scripts

--- users/xd75_lh/compile.sh
+++ users/xd75_lh/compile.sh
@@ -4,16 +4,19 @@ set -e
 
 FILE=$1
 
+WORKING_DIR=xd75_lh
+KEYMAP=lh
+
 if [ -f "$FILE" ]; then
 
   echo "1) Copying the keymap json"
-  cp $FILE $QMK_FIRMWARE_DIR/users/xd75_lh/keymap.json
+  cp $FILE $QMK_FIRMWARE_DIR/users/$WORKING_DIR/keymap.json
 
   echo "2) Converting keymap from json to c"
-  qmk json2c $FILE > $QMK_FIRMWARE_DIR/keyboards/xiudi/xd75/keymaps/lh/keymap.c
+  qmk json2c $FILE > $QMK_FIRMWARE_DIR/keyboards/xiudi/xd75/keymaps/$KEYMAP/keymap.c
     
   echo "3) Adding keymap_extra.c to keymap.c"
-  cat keymap_extra.c >> $QMK_FIRMWARE_DIR/keyboards/xiudi/xd75/keymaps/lh/keymap.c
+  cat keymap_extra.c >> $QMK_FIRMWARE_DIR/keyboards/xiudi/xd75/keymaps/$KEYMAP/keymap.c
 
   echo "4) Compiling"
   qmk compile -kb xiudi/xd75 -km lh
--- users/xd75_lh/flash.sh
+++ users/xd75_lh/flash.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+COMPILE_FILE=xiudi_xd75_lh.hex
+
 sudo dfu-programmer atmega32u4 erase --force
-sudo dfu-programmer atmega32u4 flash ../../.build/xiudi_xd75_lh.hex
+sudo dfu-programmer atmega32u4 flash $QMK_FIRMWARE_DIR/.build/$COMPILE_FILE
 sudo dfu-programmer atmega32u4 reset
